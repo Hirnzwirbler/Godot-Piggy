e15cd2c06db6411bcca5161668f4dd476d3dcb33
Hirnzwirbler <fortner.steffen@web.de>
2020-09-28 19:06:01 +0200

    Lesson 3

---
+++ Pig.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+const SPEED = 150
+
+func _process(delta):
+	if Input.is_action_pressed("ui_right"):
+		move(SPEED, 0, delta)
+	if Input.is_action_pressed("ui_left"):
+		move(-SPEED, 0, delta)
+	if Input.is_action_pressed("ui_up"):
+		move(0, -SPEED, delta)
+	if Input.is_action_pressed("ui_down"):
+		move(0, SPEED, delta)
+
+func move(xspeed, yspeed, delta):
+	position.x += xspeed * delta
+	position.y += yspeed * delta
